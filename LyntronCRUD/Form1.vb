01c9289aca7af115c35c0f37d54c396cd7398699
TheRealSlicVic <vmorellana@gmail.com>
2015-05-06 10:34:11 -0700

    Initial code commit
    Basic empty shell

---
+++ LyntronCRUD/Form1.vb
@@ -0,0 +1,28 @@
+Imports Oracle.DataAccess
+
+Public Class Form1
+    Dim oradb As String = "Data Source=LyntronSAND;User Id=ODBCUSER;Password=coffee;"
+    Dim conn As New Client.OracleConnection(oradb)
+
+    Private Sub btnClickMe_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClickMe.Click
+        Try
+            conn.Open()
+            Dim cmd As New Client.OracleCommand
+            cmd.Connection = conn
+            cmd.CommandText = "select * from GAUGES where 1 = 1"
+            cmd.CommandType = CommandType.Text
+
+            Dim dr As Client.OracleDataReader = cmd.ExecuteReader()
+
+            If dr.Read() Then
+                MessageBox.Show("Connection success. Read Success.")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString())
+        Finally
+            conn.Close()
+            conn.Dispose()
+        End Try
+    End Sub
+End Class
---
+++ LyntronCRUD/clsCRUD.vb
@@ -0,0 +1,10 @@
+Public Class clsCRUD
+
+    Public Enum CRUD_Action
+        CREATE
+        READ
+        UPDATE
+        DELETE
+    End Enum
+
+End Class
