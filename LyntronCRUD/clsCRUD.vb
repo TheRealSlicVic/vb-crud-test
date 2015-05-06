c67a508e35605ff278e227fd45554c21ed212344
TheRealSlicVic <vmorellana@gmail.com>
2015-05-06 11:14:17 -0700

    Added CRUD empty functions

--- LyntronCRUD/clsCRUD.vb
+++ LyntronCRUD/clsCRUD.vb
@@ -1,4 +1,10 @@
+Imports Oracle.DataAccess
+
 Public Class clsCRUD
+    Dim oradb As String = "Data Source=LyntronSAND;User Id=ODBCUSER;Password=coffee;"
+    Dim dbConnOracle As New Client.OracleConnection(oradb)
+    Dim cmdOracle As New Client.OracleCommand
+    Dim daDataAdapterOracle As New Odbc.OdbcDataAdapter
 
     Public Enum CRUD_Action
         CREATE
@@ -7,4 +13,37 @@ Public Class clsCRUD
         DELETE
     End Enum
 
+    Public Sub CreateRecord()
+
+    End Sub
+
+    Public Function ReadRecord(ByVal sql As String) As String
+        Dim strPartNum As String = Nothing
+
+        Try
+            dbConnOracle.Open()
+
+            With cmdOracle
+                .CommandText = Sql
+                .CommandType = CommandType.Text
+                .Connection = dbConnOracle
+            End With
+
+            daDataAdapterOracle.SelectCommand = cmdOracle
+            strPartNum = daDataAdapterOracle.SelectCommand.ExecuteScalar
+            dbConnOracle.Close()
+        Catch ex As Exception
+            'showException(ex)
+            dbConnOracle.Close()
+        End Try
+        Return strPartNum
+    End Function
+
+    Public Sub UpdateRecord()
+
+    End Sub
+
+    Public Sub DeleteRecord()
+
+    End Sub
 End Class
